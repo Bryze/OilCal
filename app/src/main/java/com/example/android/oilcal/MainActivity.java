9226b0f75f0ec17e2c4e0069e131885f529c1fce
Bryze <aseem2301@gmail.com>
2017-06-22 20:36:28 +0530

    OilCal v0.2
    1. Added reset functionality
    2. Added validations
    3.  Added string truncation to answer.

--- app/src/main/java/com/example/android/oilcal/MainActivity.java
+++ app/src/main/java/com/example/android/oilcal/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.oilcal;
 
+import android.icu.text.DecimalFormat;
 import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -20,8 +22,12 @@ public class MainActivity extends AppCompatActivity {
     public EditText oil;
     public EditText mins;
     public String his;
+    public Button reset;
     public TextView answer;
     public TextView history;
+    public double h =0;
+    public int f=0;
+    public double m = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,39 +39,89 @@ public class MainActivity extends AppCompatActivity {
         eval = (Button) findViewById(R.id.eval);
         his = "";
         answer = (TextView) findViewById(R.id.answer);
+        reset = (Button) findViewById(R.id.reset);
+        hours = (EditText) findViewById(R.id.hours);
+        mins = (EditText) findViewById(R.id.mins);
+        oil = (EditText) findViewById(R.id.oil);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                hours = (EditText) findViewById(R.id.hours);
-                mins = (EditText) findViewById(R.id.mins);
 
-                double h = Double.valueOf(hours.getText().toString());
-                  double m = Double.valueOf(mins.getText().toString());
-                double temp = 60.00;
-                result = result + h + (m/temp);
-                his = his+'\n'+(hours.getText().toString())+" hrs "+(mins.getText().toString())+" mins";
-                //String t = hours.getText().toString();
-                history.setText(his);
-                //totalTime.setText(t);
-                totalTime.setText(String.valueOf(result));
 
-                hours.setText("");
-                mins.setText("");
+                String thours = hours.getText().toString();
+                String tmins = mins.getText().toString();
+                if(thours.matches("") || tmins.matches("")) {
+                    Toast.makeText(MainActivity.this, "Enter valid value.", Toast.LENGTH_SHORT).show();
+                    hours.setText("");
+                    mins.setText("");
+                } else {
+                    h = Double.valueOf(hours.getText().toString());
+                    m = Double.valueOf(mins.getText().toString());
+                    double temp = 60.00;
+
+                    int r =check(m);
+                    if(r==1) {
+                        f=1;
+                        result = result + h + (m/temp);
+                        String display = String.format("%.2f",result);
+                        his = his+'\n'+(hours.getText().toString())+" hrs "+(mins.getText().toString())+" mins";
+                        //String t = hours.getText().toString();
+                        history.setText(his);
+                        //totalTime.setText(t);
+                        totalTime.setText(display);
+
+                        hours.setText("");
+                        mins.setText("");
+                    } else {
+                        Toast.makeText(MainActivity.this, "Insert a valid minutes range (0 to 60)", Toast.LENGTH_SHORT).show();
+                        mins.setText("");
+                    }
+                }
+
             }
         });
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hours.setText("");
+                mins.setText("");
+                answer.setText("Final Answer");
+                his="";
+                history.setText("History");
+                result = 0.00;
+                totalTime.setText("Total Time");
+                oil.setText("");
+                f=0;
 
+
+            }
+        });
         eval.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                oil = (EditText) findViewById(R.id.oil);
+                String thours = hours.getText().toString();
+                String tmins = mins.getText().toString();
+                String toil = oil.getText().toString();
+                if((thours.matches("") || tmins.matches("") || toil.matches(""))&&f==0){
+                    Toast.makeText(MainActivity.this, "Enter value to evaluate", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    double quant = Double.valueOf(oil.getText().toString());
 
-                double quant = Double.valueOf(oil.getText().toString());
-
-                double tempresult = quant/result;
-
-                answer.setText(String.valueOf(tempresult));
+                    double tempresult = quant / result;
+                    String display = String.format("%.2f",tempresult);
+                    answer.setText(display);
+                }
             }
         });
     }
+
+    private int check(double m) {
+        if(m>=0.00 && m<=60.00) {
+            return 1;
+        } else {
+            return 0;
+        }
+     }
 
 }
